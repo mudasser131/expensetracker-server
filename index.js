53a4272d0bd56274c562de4d48ab41299cf49b61
shija uddin <khajashija@gmail.com>
2025-04-18 19:59:14 +0530

    update

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 const corsOptions = {
-  origin:"http://localhost:5173",
+  origin:["http://localhost:5173","http://localhost:5174"],
   credentials:true
 }
 app.use(cors(corsOptions));
